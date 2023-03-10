5fc835d253a3f5ca4e2d338c4b45c5cd8c3ac744
DanielleSmith0 <daniellesmith@Danielles-MacBook-Pro.local>
2023-03-10 13:09:39 -0700

    initial commit-part 1 attempted

---
+++ server/controller.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["Enjoy life! It is better to be happy than wise.", "If you can't excel with talent, triumph with effort.", "You struggle for success, and it shows", "If you want to hit the target, shoot first and call what you hit the target.", "You will have 20 kids."];
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    }
+
+}
